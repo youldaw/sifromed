f6fe6f97dca133bac29ff64da343e751fb383906
youldaw <68648932+youldaw@users.noreply.github.com>
2023-10-09 23:21:31 +0500

    vfdvdfhdsg

--- js/script.js
+++ js/script.js
@@ -24,6 +24,64 @@ $(function () {
     $('.header__fixed .modal__menu').toggleClass('active');
   });
 
+  // add to card animation
+  $(".main__info-item").on("click", function () {
+    var $btn = $(this);
+    var $li = $btn.closest('.main-info-block');
+    var btnOffsetTop = $btn.offset().top;
+    var btnOffsetRight = window.innerWidth - $btn.offset().left;
+    $li.find('.icon')
+      .clone()
+      .css({ top: btnOffsetTop, right: btnOffsetRight })
+      .addClass("goo")
+      .appendTo($li);
+
+    setTimeout(function () {
+      $(".goo").remove();
+    }, 1000);
+  });
+
+
+  // show-more content
+  $(".main-info-block").slice(0, 4).show();
+  $("#show-more").on('click', function (e) {
+    e.preventDefault();
+    $(".main-info-block:hidden").slice(0, 4).slideDown();
+    if ($(".main-info-block:hidden").length == 0) {
+      $("#show-less").fadeIn('slow');
+      $("#show-more").hide();
+      // $("#loadMore").text('Load only the first 4');
+    }
+  });
+
+  $("#show-less").on('click', function (e) {
+    e.preventDefault();
+    $('.main-info-block:not(:lt(4))').fadeOut();
+    $("#show-more").fadeIn('slow');
+    $("#show-less").hide();
+  });
+
+
+  // show-more content
+  $(".info__card").slice(0, 2).show();
+  $("#load-More").on('click', function (e) {
+    e.preventDefault();
+    $(".info__card:hidden").slice(0, 2).slideDown();
+    if ($(".info__card:hidden").length == 0) {
+      $("#load-Less").fadeIn('slow');
+      $("#load-More").hide();
+      // $("#loadMore").text('Load only the first 4');
+    }
+  });
+
+  $("#load-Less").on('click', function (e) {
+    e.preventDefault();
+    $('.info__card:not(:lt(2))').fadeOut();
+    $("#load-More").fadeIn('slow');
+    $("#load-Less").hide();
+  });
+
+
 
 
   // Header scroll 
@@ -69,14 +127,6 @@ $(function () {
 });
 
 
-// rolly js
-const r = rolly({
-  view: document.querySelector('.wrapper'),
-  native: true,
-  // other options
-});
-r.init();
-
 AOS.init({
-  duration: 1500 //global duration
+  duration: 1000 //global duration
 });
