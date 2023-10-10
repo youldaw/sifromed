dc3b0de14227d3e0d2f4b10e76ba9252f9833c88
youldaw <68648932+youldaw@users.noreply.github.com>
2023-10-10 08:10:22 +0500

    fdgdfsggfsd

--- js/script.js
+++ js/script.js
@@ -14,31 +14,64 @@ $(function () {
 
 
   // Header fixed menu
-  $('.header__fixed .menu-add').on('click', function (e) {
-    e.preventDefault();
-    if ($('.header__fixed .modal__menu').hasClass('active')) {
-      $('body').css('overflow', 'auto');
-    } else {
-      $('body').css('overflow', 'hidden');
+  // stickiy header
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 1) {
+      $('header').addClass("sticky");
     }
-    $('.header__fixed .modal__menu').toggleClass('active');
+    else {
+      $('header').removeClass("sticky");
+    }
   });
 
+  // add to card counter
+  var cartCounter = 0;
+  $('.addToCart').click(function () {
+    cartCounter++;
+    var buttonCount = setTimeout(function () {
+      $('.header__menu-shopping').attr('data-count', cartCounter);
+    }, 500);
+  })
+
   // add to card animation
-  $(".main__info-item").on("click", function () {
-    var $btn = $(this);
-    var $li = $btn.closest('.main-info-block');
-    var btnOffsetTop = $btn.offset().top;
-    var btnOffsetRight = window.innerWidth - $btn.offset().left;
-    $li.find('.icon')
-      .clone()
-      .css({ top: btnOffsetTop, right: btnOffsetRight })
-      .addClass("goo")
-      .appendTo($li);
+  $('.addToCart').click(function () {
+    var target = $('.cart').first(),
+      target_offset = target.offset();
+
+    var target_x = target_offset.left,
+      target_y = target_offset.top;
+
+    console.log('target: ' + target_x + ', ' + target_y);
+
+    var obj_id = 1 + Math.floor(Math.random() * 100000),
+      obj_class = 'cart-animation-helper',
+      obj_class_id = obj_class + '_' + obj_id;
+
+    var obj = $("<div>", {
+      'class': obj_class + ' ' + obj_class_id
+    });
+
+    $(this).parent().parent().append(obj);
+
+    var obj_offset = obj.offset(),
+      dist_x = target_x - obj_offset.left + 10,
+      dist_y = target_y - obj_offset.top + 10,
+      delay = 0.8; // seconds
+
+    console.log('obj_off: ' + obj_offset.left + ', ' + obj_offset.top);
 
     setTimeout(function () {
-      $(".goo").remove();
-    }, 1000);
+      obj.css({
+        'transition': 'transform ' + delay + 's ease-in',
+        'transform': "translateX(" + dist_x + "px)"
+      });
+      $('<style>.' + obj_class_id + ':after{ \ transform: translateY(' + dist_y + 'px); \ opacity: 1; \ max-height: 20px; \ max-width: 20px; margin: 0; \ }</style>').appendTo('head');
+    }, 0);
+
+
+    obj.show(1).delay((delay + 0.02) * 1000).hide(1, function () {
+      $(obj).remove();
+    });
   });
 
 
@@ -53,7 +86,6 @@ $(function () {
       // $("#loadMore").text('Load only the first 4');
     }
   });
-
   $("#show-less").on('click', function (e) {
     e.preventDefault();
     $('.main-info-block:not(:lt(4))').fadeOut();
@@ -73,7 +105,6 @@ $(function () {
       // $("#loadMore").text('Load only the first 4');
     }
   });
-
   $("#load-Less").on('click', function (e) {
     e.preventDefault();
     $('.info__card:not(:lt(2))').fadeOut();
@@ -82,6 +113,35 @@ $(function () {
   });
 
 
+  // comment show-more content
+  $(".comment-block").slice(0, 3).show();
+  $("#show-more").on('click', function (e) {
+    e.preventDefault();
+    $(".comment-block:hidden").slice(0, 3).slideDown();
+    if ($(".comment-block:hidden").length == 0) {
+      $("#show-less").fadeIn('slow');
+      $("#show-more").hide();
+      // $("#loadMore").text('Load only the first 4');
+    }
+  });
+  $("#show-less").on('click', function (e) {
+    e.preventDefault();
+    $('.comment-block:not(:lt(3))').fadeOut();
+    $("#show-more").fadeIn('slow');
+    $("#show-less").hide();
+  });
+
+
+  // basket(cart) delete card
+  $('.delete-btn').on('click', function(){
+    $(this).parent().parent().addClass('d-none');
+  });
+
+  // basket address
+  $('.choose-address').on('click', function(){
+    $('.basket__address').addClass('active');
+    $(this).addClass('d-none');
+  });
 
 
   // Header scroll 
@@ -108,7 +168,7 @@ $(function () {
     },
     speed: 1000,
     autoplay: {
-      delay: 700,
+      delay: 1000,
     },
   });
 
@@ -119,9 +179,142 @@ $(function () {
     spaceBetween: 17,
     speed: 1000,
     autoplay: {
-      delay: 700,
+      delay: 2000,
     },
   });
+
+
+  // custom select
+  $(".custom-select").each(function () {
+    var classes = $(this).attr("class"),
+      id = $(this).attr("id"),
+      name = $(this).attr("name");
+    var template = '<div class="' + classes + '">';
+    template += '<span class="custom-select-trigger">' + $(this).attr("placeholder") + '</span>';
+    template += '<div class="custom-options">';
+    $(this).find("option").each(function () {
+      template += '<span class="custom-option ' + $(this).attr("class") + '" data-value="' + $(this).attr("value") + '">' + $(this).html() + '</span>';
+    });
+    template += '</div></div>';
+
+    $(this).wrap('<div class="custom-select-wrapper"></div>');
+    $(this).hide();
+    $(this).after(template);
+  });
+  $(".custom-option:first-of-type").hover(function () {
+    $(this).parents(".custom-options").addClass("option-hover");
+  }, function () {
+    $(this).parents(".custom-options").removeClass("option-hover");
+  });
+  $(".custom-select-trigger").on("click", function () {
+    $('html').one('click', function () {
+      $(".custom-select").removeClass("opened");
+    });
+    $(this).parents(".custom-select").toggleClass("opened");
+    event.stopPropagation();
+  });
+  $(".custom-option").on("click", function () {
+    $(this).parents(".custom-select-wrapper").find("select").val($(this).data("value"));
+    $(this).parents(".custom-options").find(".custom-option").removeClass("selection");
+    $(this).addClass("selection");
+    $(this).parents(".custom-select").removeClass("opened");
+    $(this).parents(".custom-select").find(".custom-select-trigger").text($(this).text());
+  });
+
+
+
+  // yandex map
+  ymaps.ready(init);
+
+  function init() {
+    var myMap = new ymaps.Map("map", {
+      center: [55.754960, 37.619118],
+      zoom: 11
+    }),
+      // Создаем метку с помощью вспомогательного класса.
+      myPlacemark1 = new ymaps.Placemark([55.717522, 37.554680], {
+        // Свойства.
+        // iconContent: '1',
+        // hintContent: 'Стандартный значок метки',
+        balloonContent: '<div class="map-title"><span class="map-address">Ростов-на-Дону, ул. Социалистическая, д. 238/51</span><div class="map-info"><span class="address-time">Пн-Пт: 7:30-17:00</span><span class="address-time">Сб-Вс: 8:00-14:00</span></div></div>'
+      }, {
+        // Опции.
+        // Своё изображение иконки метки.
+        iconImageHref: '../images/icons/map-point.png',
+        // Размеры метки.
+        iconImageSize: [32, 36],
+        // Смещение левого верхнего угла иконки относительно
+        // iconImageOffset: [33, -42]
+      }),
+
+      // metka 2
+      myPlacemark2 = new ymaps.Placemark([55.758189, 37.579990], {
+        // Свойства.
+        balloonContent: '<div class="map-title"><span class="map-address">Ростов-на-Дону, ул. Социалистическая, д. 238/51</span><div class="map-info"><span class="address-time">Пн-Пт: 7:30-17:00</span><span class="address-time">Сб-Вс: 8:00-14:00</span></div></div>'
+      }, {
+        // Опции.
+        // Своё изображение иконки метки.
+        iconImageHref: '../images/icons/map-point.png',
+        // Размеры метки.
+        iconImageSize: [32, 36],
+        // Смещение левого верхнего угла иконки относительно
+        // iconImageOffset: [33, -22]
+      });
+
+    // metka 3
+    myPlacemark3 = new ymaps.Placemark([55.753166, 37.631207], {
+      // Свойства.
+      // iconContent: '1',
+      balloonContent: '<div class="map-title"><span class="map-address">Ростов-на-Дону, ул. Социалистическая, д. 238/51</span><div class="map-info"><span class="address-time">Пн-Пт: 7:30-17:00</span><span class="address-time">Сб-Вс: 8:00-14:00</span></div></div>'
+    }, {
+      // Опции.
+      // Своё изображение иконки метки.
+      iconImageHref: '../images/icons/map-point.png',
+      // Размеры метки.
+      iconImageSize: [32, 36],
+      // Смещение левого верхнего угла иконки относительно
+      // iconImageOffset: [33, -42]
+    }),
+
+      // metka 4
+      myPlacemark4 = new ymaps.Placemark([55.723732, 37.688150], {
+        // Свойства.
+        // iconContent: '1',
+        balloonContent: '<div class="map-title"><span class="map-address">Ростов-на-Дону, ул. Социалистическая, д. 238/51</span><div class="map-info"><span class="address-time">Пн-Пт: 7:30-17:00</span><span class="address-time">Сб-Вс: 8:00-14:00</span></div></div>'
+      }, {
+        // Опции.
+        // Своё изображение иконки метки.
+        iconImageHref: '../images/icons/map-point.png',
+        // Размеры метки.
+        iconImageSize: [32, 36],
+        // Смещение левого верхнего угла иконки относительно
+        // iconImageOffset: [33, -42]
+      }),
+
+      // metka 5
+      myPlacemark5 = new ymaps.Placemark([55.765363, 37.704374], {
+        // Свойства.
+        // iconContent: '1',
+        balloonContent: '<div class="map-title"><span class="map-address">Ростов-на-Дону, ул. Социалистическая, д. 238/51</span><div class="map-info"><span class="address-time">Пн-Пт: 7:30-17:00</span><span class="address-time">Сб-Вс: 8:00-14:00</span></div></div>'
+      }, {
+        // Опции.
+        // Своё изображение иконки метки.
+        iconImageHref: '../images/icons/map-point.png',
+        // Размеры метки.
+        iconImageSize: [32, 36],
+        // Смещение левого верхнего угла иконки относительно
+        // iconImageOffset: [33, -42]
+      }),
+
+      // Добавляем все метки на карту.
+      myMap.geoObjects
+        .add(myPlacemark1)
+        .add(myPlacemark2)
+        .add(myPlacemark3)
+        .add(myPlacemark4)
+        .add(myPlacemark5);
+  }
+
 
 
 });
@@ -130,3 +323,14 @@ $(function () {
 AOS.init({
   duration: 1000 //global duration
 });
+
+
+
+
+
+
+
+
+
+
+
